7b2450443dbb155ceb5825ac13267467c7ed1fc5
sentix GmbH <webmaster@sentix.de>
2018-12-23 18:58:03 +0100

    Test

--- site/controllers/ajax.json.php
+++ site/controllers/ajax.json.php
@@ -9,6 +9,8 @@
  * @license    GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html
  */
 
+ echo "Test";
+
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
 
